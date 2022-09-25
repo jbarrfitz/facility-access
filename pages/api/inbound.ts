acc340f41a2a1bedf1a347d993ef9117e81d62c6
jerry.barrows@gmail.com <jerry.barrows@gmail.com>
2022-09-25 17:46:36 -0500

    feat: strong typing

--- pages/api/inbound.ts
+++ pages/api/inbound.ts
@@ -1,66 +1,68 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { twiml } from 'twilio';
-import { google } from 'googleapis'
+import { google, calendar_v3 } from 'googleapis'
 import { InboundSMSRequest } from '../../types/sms';
 const { MessagingResponse } = twiml;
+
+const calendarId =
+    'a7f4549fe542d67b14dbca0debd3c0c3ac9a4686f8cfccb3cc9e0610f5cbcedc@group.calendar.google.com';
 
 export default async function inboundMessage(
   req: InboundSMSRequest,
   res: NextApiResponse
 ) {
   const messages = [];
-  const cal = google.calendar({
-    version: 'v3',
-    auth: 'AIzaSyBuYDZj2vbbDtW1XwnyhhDBEX9TdhR_zHM',
-  });
-
-  const calendar =
-    'a7f4549fe542d67b14dbca0debd3c0c3ac9a4686f8cfccb3cc9e0610f5cbcedc@group.calendar.google.com';
 
   const enterTime = new Date();
   enterTime.setSeconds(enterTime.getSeconds() - 60);
   let exitTime = new Date();
   exitTime.setSeconds(enterTime.getSeconds() + 60);
 
-  const currentEvents = await cal.freebusy
-  .query({
-      requestBody: {
-        // Set times to ISO strings as such
-        timeMin: enterTime.toISOString(),
-        timeMax: exitTime.toISOString(),
-        timeZone: 'US/Central',
-        items: [{ id: calendar }],
-      },
-    })
-    .then((result: any) => {
-      console.warn('RESULT', result);
-      const busy = result.data.calendars[calendar].busy;
-      const errors = result.data.calendars[calendar].errors;
-      if (errors) {
-        throw new Error(errors);
-      }
-      return busy;
-    })
-    .catch((e: any) => {
-      console.error(e);
-      // return callback(e);
+  let facilityOpen = false;
+
+  try {
+    const calApi = google.calendar({
+      version: 'v3',
+      auth: 'AIzaSyBuYDZj2vbbDtW1XwnyhhDBEX9TdhR_zHM',
     });
+
+    const calQueryRes = await calApi.freebusy
+      .query({
+        requestBody: {
+          // Set times to ISO strings as such
+          timeMin: enterTime.toISOString(),
+          timeMax: exitTime.toISOString(),
+          timeZone: 'US/Central',
+          items: [{ id: calendarId }],
+        },
+      })
+    const { busy, errors } = calQueryRes.data.calendars?.[calendarId] as calendar_v3.Schema$FreeBusyCalendar
+    if (errors) {
+      throw new Error(errors.toString());
+    } else {
+      // Facility is open if there are planned events
+      facilityOpen = !!busy?.length
+    }
+  } catch (err) {
+    // ...handle error
+    messages.push('An error has occured, please try again later')
+  }
 
   // Use any of the Node.js SDK methods, such as `message`, to compose a response
   // In this case we're also including the doge image as a media attachment
   // Note: access incoming text details such as the from number on `event`
 
-  if (currentEvents.length !== 0) {
-    messages.push('[DOOR UNLOCKED]');
-    messages.push('Welcome to Grizzly Bear Auditorium! Enjoy your visit.');
-    messages.push('Please clean any equipment you use before you leave.');
-    messages.push('If you like our place, please leave us a review on Google.');
-  } else {
-    messages.push('GRIZZLY BEAR:');
-    messages.push(
-      "We couldn't find a current reservation under this phone number, please double check your booking email and verify your checkin time."
-    );
-  }
+  // if (currentEvents.length !== 0) {
+  //   messages.push('[DOOR UNLOCKED]');
+  //   messages.push('Welcome to Grizzly Bear Auditorium! Enjoy your visit.');
+  //   messages.push('Please clean any equipment you use before you leave.');
+  //   messages.push('If you like our place, please leave us a review on Google.');
+  // } else {
+  //   messages.push('GRIZZLY BEAR:');
+  //   messages.push(
+  //     "We couldn't find a current reservation under this phone number, please double check your booking email and verify your checkin time."
+  //   );
+  // }
 
   // Return the TwiML as the second argument to `callback`
   // This will render the response as XML in reply to the webhook request
@@ -72,7 +74,6 @@ export default async function inboundMessage(
 
   // twiml.message('The Robots are coming! Head for the hills!');
 
-  // res.writeHead(200, { 'Content-Type': 'text/xml' });
-  // res.end(twiml.toString());
-  res.status(200).json({ messages });
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
 }
